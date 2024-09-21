29243e4f04c71a6aa7703d80f4e4b5d1b247f7b8
rachelwu <rachel9060@gmail.com>
2024-09-21 11:26:11 +0800

    added fib.js in week-02 folder

---
+++ week-02/fib.js
@@ -0,0 +1,12 @@
+//以下為遞迴實作Fibonacci 函數
+function fib(n){
+    if (n<=1)
+        return n;   //case:fib(0)=0, fib(1)=1
+
+    return fib(n)=fib(n-1)=fib(n-2); //下個數字總會是前兩個數字的和
+}
+
+console.log(fib(0));  // 0
+console.log(fib(1));  // 1
+console.log(fib(5));  // 5
+console.log(fib(10)); // 55
