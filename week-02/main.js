4d87c226defea7df47cc17062aa39454a9407866
rachelwu <rachel9060@gmail.com>
2024-09-27 01:55:43 +0800

    revised main.js in week-02 folder

--- week-02/main.js
+++ week-02/main.js
@@ -9,7 +9,7 @@ myStack.push(6); //將6放入堆疊
 
 myStack.print(); // 印出 9,0,6
 
-myStack.pop(); // 移除頂端元素 6
+myStack.pop(); // 移除並回傳頂端元素 6
 myStack.print(); // 印出 9,0
 
 myStack.push(4); //將4放入堆疊
@@ -19,7 +19,8 @@ myStack.push(99); //將99放入堆疊
 
 /*除了基本的push、pop之外，也須驗證堆疊為滿或為空時，
 是否會在行為後顯示錯誤。例如堆疊已滿時無法再插入新的元素*/
-myStack.push(10); //在堆疊已滿時，插入元素，應顯示堆疊已滿
+myStack.push(10); //嘗試在堆疊已滿時，插入元素，應顯示堆疊已滿
+console.log(myStack.size());  // 應該顯示 5，證明沒有插入第6個元素
 
 myStack.clear(); // 清空堆疊
 myStack.print(); // 印出空字串
